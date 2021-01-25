93418d5f5382bef1d57f536fb152d7413e738c4a
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-25 14:51:09 -0600

    Initial commit

---
+++ vTypes/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.btnAverage = New System.Windows.Forms.Button()
+        Me.btnChar = New System.Windows.Forms.Button()
+        Me.btnSingle = New System.Windows.Forms.Button()
+        Me.btnInteger = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.AutoSize = True
+        Me.lblDisplay.Location = New System.Drawing.Point(30, 268)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(41, 13)
+        Me.lblDisplay.TabIndex = 14
+        Me.lblDisplay.Text = "Display"
+        '
+        'btnAverage
+        '
+        Me.btnAverage.Location = New System.Drawing.Point(12, 204)
+        Me.btnAverage.Name = "btnAverage"
+        Me.btnAverage.Size = New System.Drawing.Size(75, 23)
+        Me.btnAverage.TabIndex = 13
+        Me.btnAverage.Text = "Average"
+        Me.btnAverage.UseVisualStyleBackColor = True
+        '
+        'btnChar
+        '
+        Me.btnChar.Location = New System.Drawing.Point(12, 140)
+        Me.btnChar.Name = "btnChar"
+        Me.btnChar.Size = New System.Drawing.Size(75, 23)
+        Me.btnChar.TabIndex = 12
+        Me.btnChar.Text = "Char"
+        Me.btnChar.UseVisualStyleBackColor = True
+        '
+        'btnSingle
+        '
+        Me.btnSingle.Location = New System.Drawing.Point(12, 76)
+        Me.btnSingle.Name = "btnSingle"
+        Me.btnSingle.Size = New System.Drawing.Size(75, 23)
+        Me.btnSingle.TabIndex = 11
+        Me.btnSingle.Text = "Single"
+        Me.btnSingle.UseVisualStyleBackColor = True
+        '
+        'btnInteger
+        '
+        Me.btnInteger.Location = New System.Drawing.Point(12, 12)
+        Me.btnInteger.Name = "btnInteger"
+        Me.btnInteger.Size = New System.Drawing.Size(75, 23)
+        Me.btnInteger.TabIndex = 10
+        Me.btnInteger.Text = "Integer"
+        Me.btnInteger.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1157, 449)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.btnAverage)
+        Me.Controls.Add(Me.btnChar)
+        Me.Controls.Add(Me.btnSingle)
+        Me.Controls.Add(Me.btnInteger)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblDisplay As System.Windows.Forms.Label
+    Friend WithEvents btnAverage As System.Windows.Forms.Button
+    Friend WithEvents btnChar As System.Windows.Forms.Button
+    Friend WithEvents btnSingle As System.Windows.Forms.Button
+    Friend WithEvents btnInteger As System.Windows.Forms.Button
+
+End Class
---
+++ vTypes/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+
+    Private Sub btnInteger_Click(sender As Object, e As EventArgs) Handles btnInteger.Click
+
+        ' A variable is a container for information
+        ' Dim= dimension. sets space for the variable
+        ' intNumber is the label (name)
+        ' Integer is the type of the Variable
+
+        Dim intNumber1 As Integer
+        Dim intNumber2 As Integer
+        Dim intAnswer As Integer
+
+        intNumber1 = 7
+        ' NOT an equation, this is an action
+        ' intNumber1 "Get the Value" 7
+        intNumber2 = 2.4
+        ' Integers cannot handle decimals
+        ' They will just disappear...
+        ' intNumber2 is 2 NOT 2.4
+        intAnswer = intNumber1 / intNumber2
+        ' / rounds off values
+
+        lblDisplay.Text = "Answer: " & intAnswer
+    End Sub
+
+    Private Sub btnSingle_Click(sender As Object, e As EventArgs) Handles btnSingle.Click
+
+        Dim sngNumber1 As Single
+        Dim sngNumber2 As Single
+        Dim sngAnswer As Single
+        ' Variable names cannot conatin spaces!
+        ' MUST start with a letter
+        ' CAN contain a number (just not at start)
+        ' written in camelCase
+
+        sngNumber1 = 7
+        sngNumber2 = 2.4
+        sngAnswer = sngNumber1 / sngNumber2
+        lblDisplay.Text = "Answer: " & sngAnswer
+        ' Singles are numbers that can handle decimals
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnChar.Click
+        Dim chaLetter1 As Char
+        ' a Char can hold a single "character"
+        ' a letter, a number(single digit), space
+        Dim chaLetter2 As Char
+        ' Green squiggle is a warning
+        ' This one is for an unused variable
+
+        chaLetter1 = "A"
+
+        lblDisplay.Text = chaLetter1
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnAverage.Click
+        Dim sngNum1 As Single = 78
+        Dim sngNum2 As Single = 64
+        Dim sngNum3 As Single = 89
+        Dim sngNum4 As Single = 91
+        ' variable assignment upon declaration
+
+        Dim sngAverage As Single
+        sngAverage = (sngNum1 + sngNum2 + sngNum3 + sngNum4) / 4
+        ' BODMAS
+        ' Brackets
+        ' Orders(exponents, squareRoots)
+        ' Division
+        ' Multiplication
+        ' Addition
+        ' Subtractions
+
+        lblDisplay.Text = sngAverage
+
+    End Sub
+
+    Private Sub lblDisplay_Click(sender As Object, e As EventArgs) Handles lblDisplay.Click
+
+    End Sub
+End Class
